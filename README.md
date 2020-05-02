6e074357c56072cf3b2ccaefacca7d6f73f63d3b
Maximiliano Churichi <mchurichi@gmail.com>
2020-05-02 17:13:34 -0300

    Agregado arparse y la posibilidad de descargar en format epub

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # SpringerBooks
-Script de Python para bajar los libros que `Springer` libera por `COVID-19`.
+Script de Python para bajar los libros que [`Springer` libera por `COVID-19`.](https://link.springer.com/search/facetexpanded/discipline?facet-content-type=%22Book%22&package=mat-covid19_textbooks)
 
 Para ejecutarlo:
 
 ```bash
-python main.py <ruta donde descargar los libros>
+python main.py [--pdf] [--epub] <ruta donde descargar los libros>
 ```
 
-Va a generar una carpeta por Disciplina y dentro de cada carpeta va a guardar los pdf con nombre (_Autores_) _Título_.pdf
+Para ayuda:
+
+```bash
+python main.py --help
+```
+
+Va a generar un directorio por Disciplina y dentro de cada directorio va a guardar los archivos con nombre (_Autores_) _Título_.pdf o .epub
 
 Son 400 y algo de libros.
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import bs4
 import urllib
 import sys
+import argparse
 from math import ceil
 from tqdm import tqdm
 from unicodedata import normalize
@@ -12,66 +13,96 @@ from unicodedata import normalize
 books_per_page = 10
 url_categories = 'https://link.springer.com/search/facetexpanded/discipline?facet-content-type=%22Book%22&package=mat-covid19_textbooks'
 urlbase = 'https://link.springer.com/'
-if len(sys.argv)==1:
-    destination=os.path.dirname(os.path.realpath(__file__))
-else:
-    destination=sys.argv[1]
+
 # %%
 def getCategories(url_categories):
     res = requests.get(url_categories)
-    content = bs4.BeautifulSoup(res.text,'html.parser')
+    content = bs4.BeautifulSoup(res.text, 'html.parser')
     number_of_classes = int(content.select_one('.number-of-pages').text)
     categories = []
-    for c in range(1,number_of_classes+1):
-        res = requests.get(url_categories+f'&page={c}')
-        content = bs4.BeautifulSoup(res.text,'html.parser')
+
+    for c in range(1, number_of_classes + 1):
+        res = requests.get(f'{url_categories}&page={c}')
+        content = bs4.BeautifulSoup(res.text, 'html.parser')
         category_buffer = [x.select_one('a').text.split('\n')[2] for x in content.select_one('ol').select('li')]
         categories.extend(category_buffer)
     return categories
 
-def downloadCategory(category,urlbase,destination):
+def downloadCategory(category, urlbase, destination, media_types):
     query = 'facet-content-type=' + urllib.parse.quote_plus('\"Book\"') + '&just-selected-from-overlay-value=' + urllib.parse.quote_plus(f'\"{category}\"') + '&just-selected-from-overlay=facet-discipline&package=mat-covid19_textbooks&facet-discipline='+ urllib.parse.quote_plus(f'\"{category}\"')
     url_category = f'https://link.springer.com/search?{query}'
     res = requests.get(url_category)
-    content = bs4.BeautifulSoup(res.text,'html.parser')
+    content = bs4.BeautifulSoup(res.text, 'html.parser')
     number_of_books = int(content.select_one('#number-of-search-results-and-search-terms').select_one('strong').text)
-    if number_of_books>0:
-        print(f'Downloading {number_of_books} book for {category}')
-        number_of_pages = ceil(number_of_books/books_per_page)#int(content.select_one('.number-of-pages').text)
+
+    if number_of_books > 0:
+        print(f'Downloading {number_of_books} book(s) for {category}')
+        number_of_pages = ceil(number_of_books / books_per_page)
         book_list=[]
+
         with tqdm(total=number_of_books) as pbar:
-            for n in range(1,number_of_pages+1):
+            for n in range(1, number_of_pages + 1):
                 url_category = f'https://link.springer.com/search/page/{n}?{query}'
                 res = requests.get(url_category)
-                content = bs4.BeautifulSoup(res.text,'html.parser')
+                content = bs4.BeautifulSoup(res.text, 'html.parser')
                 book_list = [x.select_one('a').get_attribute_list('href')[0] for x in content.select_one('.content-item-list').select('h2')]
                 for book_url in book_list:
-                    downloadBook(urlbase+book_url,category,urlbase,destination)
+                    downloadBook(urlbase+book_url, category, urlbase, destination, media_types)
                     pbar.update(1)
 
-def downloadBook(book_url,category,urlbase,destination):
+def downloadBook(book_url, category, urlbase, destination, media_types):
     res = requests.get(book_url)
     content = bs4.BeautifulSoup(res.text,'html.parser')
-    title = content.select_one('.page-title').select_one('h1').text
-    authors = '; '.join([normalize('NFKC',x.text) for x in content.select('.authors__name')])
-    try:
-        download_link = content.select_one('a[data-track-action=\"Book download - pdf\"]').get_attribute_list('href')[0]
-        if not os.path.exists(os.path.join(destination,category.replace('/','-'))):
-            os.mkdir(os.path.join(destination,category.replace('/','-')))
-        if not os.path.exists(os.path.join(destination,category.replace('/','-'),'(' + authors.replace('/','_') +') ' + title.replace('/','_') +'.pdf')):
-            r = requests.get(urlbase + download_link)
-            with open(os.path.join(destination,category.replace('/','-'),'(' + authors.replace('/','_') +') ' + title.replace('/','_') +'.pdf'),'wb') as f:
-                f.write(r.content)
-    except:
-        pass
+    title = content.select_one('.page-title').select_one('h1').text.replace('/','_')
+    authors = '; '.join([normalize('NFKC', x.text) for x in content.select('.authors__name')]).replace('/', '_')
+
+    for media_type in media_types:
+        try:
+            download_link = content.select_one(f'a[data-track-action="Book download - {media_type}"]').get_attribute_list('href')[0]
+            download_dir_path = os.path.join(destination, category.replace('/','-'))
+            download_full_path = os.path.join(download_dir_path, f'({authors}) {title}.{media_type.lower()}')
+
+            if not os.path.exists(download_dir_path):
+                os.mkdir(download_dir_path)
+            if not os.path.exists(download_full_path):
+                r = requests.get(urlbase + download_link)
+                with open(download_full_path, 'wb') as f:
+                    f.write(r.content)
+        except:
+            pass
 
 # %%
-if os.path.exists(destination):
-    print(f'Downloading books into {destination}')
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Downloads Springer free ebooks')
+    parser.add_argument('destination', action='store',
+                        help='Path where the files will be downloaded')
+    parser.add_argument('--pdf', dest='media_types', action='append_const', const='pdf',
+                        help='Downloads PDF version of the books')
+    parser.add_argument('--epub', dest='media_types', action='append_const', const='ePub',
+                        help='Downloads EPUB version of the books')
+
+    args = parser.parse_args()
+
+    if args.media_types is None:
+        parser.error('--pdf and/or --epub must be set')
+
+    return args
+
+def run(args):
+    if not os.path.exists(args.destination):
+        os.mkdir(args.destination)
+
+    print(f'Downloading books into {args.destination}')
     categories = getCategories(url_categories)
 
     for category in categories:
-        downloadCategory(category,urlbase,destination)
+        downloadCategory(category.replace('/','-'), urlbase, args.destination, args.media_types)
+
     print('Done!!!')
-else:
-    print(f'Path {destination} doesn\'t exist')
+
+def main():
+    args = parseArgs()
+    run(args)
+
+if __name__ == '__main__':
+    main()
